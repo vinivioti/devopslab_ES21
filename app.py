75262e9091ff356b03102ea283e2a13c5259b539
vinivioti <82620421+vinivioti@users.noreply.github.com>
2021-11-03 19:48:40 -0300

    CSRF

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
+csrf = CSRFProtect(app)
+
 @app.route("/")
 def pagina_inicial():
-    return "Vou dominar essa ferramenta, acredite !!"
+    return "Mensagem Personalizada Vinissius"
 
 if __name__ == '__main__':
     app.run(debug=True)
--- test.py
+++ test.py
@@ -17,4 +17,4 @@ class Test(unittest.TestCase):
 
     def test_conteudo(self):                                                                                                                                                                             
         # verifica o retorno do conteudo da pagina                                                                                                                                                       
-        self.assertEqual(self.result.data.decode('utf-8'), "Vou dominar essa ferramenta, acredite !!")
+        self.assertEqual(self.result.data.decode('utf-8'), "Mensagem Personalizada Vinissius")
